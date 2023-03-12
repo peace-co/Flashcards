4b41a309001643a68d84e73cbd9778a51ea68077
peace-co <127666443+peace-co@users.noreply.github.com>
2023-03-13 00:07:30 +0100

    Fix up flashcard pages

--- src/main/webapp/app/entities/flashcard/detail/flashcard-detail.component.ts
+++ src/main/webapp/app/entities/flashcard/detail/flashcard-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { ITag } from 'app/entities/tag/tag.model';
 
 import { IFlashcard } from '../flashcard.model';
 
@@ -9,12 +10,15 @@ import { IFlashcard } from '../flashcard.model';
 })
 export class FlashcardDetailComponent implements OnInit {
   flashcard: IFlashcard | null = null;
+  tags: ITag[] | null = null;
 
   constructor(protected activatedRoute: ActivatedRoute) {}
 
   ngOnInit(): void {
     this.activatedRoute.data.subscribe(({ flashcard }) => {
       this.flashcard = flashcard;
+      this.tags = flashcard.tags;
+      console.log('this.tags: ', this.tags);
     });
   }
 
